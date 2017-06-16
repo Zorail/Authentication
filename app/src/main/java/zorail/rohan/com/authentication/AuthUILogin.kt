c097295dcd6914de8385c7008a38889b36f969f6
Rohan Seth <Rohan Seth>
2017-06-16 16:00:48 +0530

    firebaseUI added

---
+++ app/src/main/java/zorail/rohan/com/authentication/AuthUILogin.kt
@@ -0,0 +1,76 @@
+package zorail.rohan.com.authentication
+
+import android.content.Intent
+import android.os.Bundle
+import android.support.annotation.MainThread
+import android.support.v7.app.AppCompatActivity
+import android.view.View
+import com.firebase.ui.auth.AuthUI
+import com.firebase.ui.auth.IdpResponse
+import com.firebase.ui.auth.ResultCodes
+import com.google.android.gms.auth.api.Auth
+import com.google.android.gms.tasks.OnCompleteListener
+import com.google.android.gms.tasks.Task
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseUser
+import kotlinx.android.synthetic.main.firebaseui_phone.*
+
+/**
+ * Created by zorail on 16-Jun-17.
+ */
+class AuthUILogin:AppCompatActivity(){
+   lateinit var mAuth:FirebaseAuth
+    companion object{
+        const val RC_SIGN_IN = 100
+    }
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.firebaseui_phone)
+        mAuth = FirebaseAuth.getInstance()
+        button_start_verification.setOnClickListener { signIn() }
+        signed_in_buttons.setOnClickListener { signOut() }
+
+    }
+    fun signIn():Unit{
+        startActivityForResult(AuthUI.getInstance()
+                .createSignInIntentBuilder().setTheme(AuthUI.getDefaultTheme())
+                .setLogo(AuthUI.NO_LOGO)
+                .setAvailableProviders(getProvider())
+                .build(), RC_SIGN_IN)
+    }
+    fun getProvider():List<AuthUI.IdpConfig>{
+        val selectedProviders = ArrayList<AuthUI.IdpConfig>()
+        selectedProviders.add(AuthUI.IdpConfig.Builder(AuthUI.PHONE_VERIFICATION_PROVIDER).build())
+        return selectedProviders
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if (requestCode == RC_SIGN_IN) {
+            handleSignInResponse(resultCode, data)
+        }
+    }
+    @MainThread
+    fun handleSignInResponse(resultCode: Int,data:Intent?){
+        val user:FirebaseUser? = mAuth.currentUser
+        if(resultCode == ResultCodes.OK)
+            if(user!=null) {
+                detail.text = user.uid
+                status.text = getString(R.string.signed_in)
+                button_start_verification.visibility = View.GONE
+                signed_in_buttons.visibility = View.VISIBLE
+            }
+
+
+    }
+    @MainThread
+    fun signOut(){
+        AuthUI.getInstance().signOut(this)
+                .addOnCompleteListener {
+                    status.text = getString(R.string.signed_out)
+                    detail.text = null
+                    signed_in_buttons.visibility = View.GONE
+                    button_start_verification.visibility =View.VISIBLE
+                }
+    }
+}
